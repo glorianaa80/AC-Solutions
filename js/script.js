62e2149f3e3467595c8dbbd5ed435332b284666d
glorianaa80 <glorianaa80@gmail.com>
2020-07-05 14:50:26 -0600

    Added submenu functionality

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-const plantsLi = document.getElementById('plants-link');
-const menueLevel = document.getElementById('plants-accordion');
+const list = document.getElementById('link');
+const menueLevel = document.getElementById('accordion');
+
+function accordion() {
+    if (list) {
+        menueLevel.classList.remove('esconde');
+        menueLevel.classList.add('muestra');
+    } else {
+        menueLevel.classList.remove('muestra');
+        menueLevel.classList.add('esconde');
+        console.log('hola no ');
+    }
+}
+list.addEventListener('click', accordion);
