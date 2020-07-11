6198716d631d1ede5d716b76e61e0df539b47308
glorianaa80 <glorianaa80@gmail.com>
2020-07-10 22:08:01 -0600

    Added menu function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-const list = document.getElementById('link');
-const menueLevel = document.getElementById('accordion');
+// const list = document.getElementById('link');
+// const menueLevel = document.getElementById('accordion');
 
-function accordion() {
-    if (list) {
-        menueLevel.classList.remove('esconde');
-        menueLevel.classList.add('muestra');
-    } else {
-        menueLevel.classList.remove('muestra');
-        menueLevel.classList.add('esconde');
-        console.log('hola no ');
-    }
+// function accordion() {
+//     if (list) {
+//         menueLevel.classList.remove('esconde');
+//         menueLevel.classList.add('muestra');
+//     } else {
+//         menueLevel.classList.remove('muestra');
+//         menueLevel.classList.add('esconde');
+//         console.log('hola no ');
+//     }
+// }
+// list.addEventListener('click', accordion);
+function classChange() {
+    let siteNav = document.getElementById('site-nav');
+    let menuOpen = document.getElementById('menu-toggle');
+    siteNav.classList.toggle('site-nav-open');
+    menuOpen.classList.toggle('menu-open')
 }
-list.addEventListener('click', accordion);
