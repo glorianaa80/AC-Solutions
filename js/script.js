93795c9e585644a507a8cc50ccb0dacaa2b178c2
glorianaa80 <glorianaa80@gmail.com>
2020-07-19 19:17:38 -0600

    Delete comments

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// const list = document.getElementById('link');
-// const menueLevel = document.getElementById('accordion');
-
-// function accordion() {
-//     if (list) {
-//         menueLevel.classList.remove('esconde');
-//         menueLevel.classList.add('muestra');
-//     } else {
-//         menueLevel.classList.remove('muestra');
-//         menueLevel.classList.add('esconde');
-//         console.log('hola no ');
-//     }
-// }
-// list.addEventListener('click', accordion);
 function classChange() {
     let siteNav = document.getElementById('site-nav');
     let menuOpen = document.getElementById('menu-toggle');
